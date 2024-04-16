9c243f3e6944a8cb0826e796cbf8e7973bae4950
wagner <wagneroliveirarodrigues@gmail.com>
2024-04-16 19:54:14 -0300

    init: primeiro commit

---
+++ application/src/main/java/com/fullcycle/admin/catalago/application/UseCase.java
@@ -0,0 +1,12 @@
+package com.fullcycle.admin.catalago.application;
+
+import com.fullcycle.admin.catalago.domain.Category;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class UseCase {
+
+    public Category execute() {
+        return new Category();
+    }
+}
---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/categoryTest/CategoryTest.java
@@ -0,0 +1,31 @@
+package com.fullcycle.admin.catalogo.domain.categoryTest;
+import com.fullcycle.admin.catalago.domain.category.Category;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class CategoryTest {
+
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+    }
+
+}
