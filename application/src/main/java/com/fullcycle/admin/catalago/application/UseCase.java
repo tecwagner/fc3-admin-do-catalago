37744ee9f430b5338bc1849bde9d84c56e357e0d
Wagner <wagner.oliveira@plataformax8.com.br>
2024-04-30 20:37:17 -0300

    feat: created entity category to project

--- application/src/main/java/com/fullcycle/admin/catalago/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalago/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.fullcycle.admin.catalago.application;
 
-import com.fullcycle.admin.catalago.domain.Category;
+import com.fullcycle.admin.catalago.domain.category.Category;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
